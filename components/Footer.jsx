52f9fe0357299af7212394f9f6fe9e39e45de5d1
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-05-28 02:41:33 -0400

    Added text on homepage. Add link to AboutMe page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import '../src/App.css';
 const Footer = () => {
     return (
         <footer  className="footer" style={{background: '#333', color: '#fff', textAlign: 'center', padding: '10px'}}>
-            <p>Test footer</p>
+            <p>2024 Janiel Javier. All rights reserved.</p>
         </footer>
     );
 }
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 
-export default function Home()  {return (
-	<>
-      <h1 id="welcome-text">Welcome!!</h1>
-        <Link id="about-me-link" to="/about">About Me</Link>
-        
-	</>
+export default function Home() {
+	return (
+		<>
+			<h1 className="welcome-text">Hello there! Welcome to my page</h1>
+			<p className="portfolio-intro">This is my personal portfolio website. Feel free to explore!</p>
+			<p className="mission-statement">
+				<strong>Mission Statement:</strong> My mission is to leverage my skills and experiences in software development to create impactful and user-friendly applications. I am committed to continuous learning and growth, and strive to make a positive difference in the tech industry.
+			</p>
+			<Link className="get-to-know-me-link" to="/about">Get to know me!</Link>
+		</>
 
-)}
+	)
+}
