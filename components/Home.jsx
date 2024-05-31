68ef953ff88b2bfaf2d4a793750944cd1c003205
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-05-31 01:02:46 -0400

    Update Homepage formatting, texts, etc

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
+import '../src/home.css'
 
 export default function Home() {
-	return (
-		<>
-			<h1 className="welcome-text">Hello there! Welcome to my page</h1>
-			<p className="portfolio-intro">This is my personal portfolio website. Feel free to explore!</p>
-			<p className="mission-statement">
-				<strong>Mission Statement:</strong> My mission is to leverage my skills and experiences in software development to create impactful and user-friendly applications. I am committed to continuous learning and growth, and strive to make a positive difference in the tech industry.
-			</p>
-			<Link className="get-to-know-me-link" to="/about">Get to know me!</Link>
-		</>
+	return <>
 
-	)
+<section id="intro">
+          <div className="homeIntro">
+          <span className="homeHello">Hello there!</span><br/>
+          <span className="introText">I am <span className="introName">Janiel Mark Javier</span> 
+
+          <br/><br/><span className="jobTitle">Software Engineer</span></span>
+		  <p className="mission-statement">
+				<strong>Mission Statement:</strong> My mission is to leverage my skills and experiences 
+				<br/> in software development to create impactful and user-friendly applications. 
+				<br/> I am committed to continuous learning and growth, and strive to make a 
+				<br/> positive difference in the tech industry.
+			</p>
+	 <Link className="get-to-know-me-link" to="/about">Get to know me!</Link>
+
+          </div>
+          
+     </section>
+
+	 </>
 }
