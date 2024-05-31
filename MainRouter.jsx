4eaa3bf1b105ddb906f35b81400ab3f94223000c
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-05-31 14:53:19 -0400

    Updated home page to increase height to always show "get to know me" link

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,3 +1,10 @@
+/*
+File name: MainRouter.jsx
+Student Name: Janiel Mark Javier
+Student ID: 301379377
+Date: 05/31/2024
+*/
+
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
